dab7c55a0ec0f066ce2ba3994855dc0389441cdb
oscar90y5 <oscarfernandezangulo@gmail.com>
2016-11-17 17:35:11 +0100

    Funciona la reproduccion de los videos de la lista

--- app/src/main/java/es/uva/mangostas/sharedplaylist/MainActivity.java
+++ app/src/main/java/es/uva/mangostas/sharedplaylist/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,27 +17,35 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerFragment;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements NewFruitDialogFragment.NewFruitDialogListner{
-    ListView listView ;
+public class MainActivity extends AppCompatActivity implements NewFruitDialogFragment.NewFruitDialogListner, YouTubePlayer.OnInitializedListener, YouTubePlayer.PlayerStateChangeListener {
+    ListView listView;
     ArrayAdapter<String> adapter;
+    private YouTubePlayer youTubePlayer;
+    private ArrayList<String> playList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // Get ListView object from xml
         listView = (ListView) findViewById(R.id.listView);
 
         // Defined Array playList to show in ListView
-        ArrayList<String> playList = new ArrayList<>();
+        playList = new ArrayList<>();
 
         //CANCIONES DE PRUEBA
         playList.add("OBXRJgSd-aU");
         playList.add("0rEVwwB3Iw0");
+
 
         // Define a new Adapter
         // First parameter - Context
@@ -59,21 +68,21 @@ public class MainActivity extends AppCompatActivity implements NewFruitDialogFra
                                     int position, long id) {
 
                 // ListView Clicked item index
-                int itemPosition     = position;
+                int itemPosition = position;
 
                 // ListView Clicked item value
-                String  itemValue    = (String) listView.getItemAtPosition(position);
+                String itemValue = (String) listView.getItemAtPosition(position);
 
                 // Show Alert
                 Toast.makeText(getApplicationContext(),
-                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
                         .show();
 
             }
 
         });
 
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -85,10 +94,8 @@ public class MainActivity extends AppCompatActivity implements NewFruitDialogFra
         });
 
         //Iniciamos el hilo del controlador de Youtube
-        YouTubePlayerFragment fragment=(YouTubePlayerFragment)getFragmentManager().findFragmentById(R.id.youtubeFragment);
-        YoutubePlayerControler youtubePlayerControler = new YoutubePlayerControler(fragment, playList);
-        youtubePlayerControler.start();
-
+        YouTubePlayerFragment fragment = (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtubeFragment);
+        fragment.initialize("AIzaSyASYbIO42ecBEzgB5kiPpu2OHJV8_5ulnk", this);
     }
 
     @Override
@@ -118,9 +125,23 @@ public class MainActivity extends AppCompatActivity implements NewFruitDialogFra
         DialogFragment dialog = new NewFruitDialogFragment();
         dialog.show(getFragmentManager(), "Fruta");
     }
+
+    public void nextVideo(){
+
+        if(playList.size()!=0) {
+
+            String video = adapter.getItem(0);
+            adapter.remove(video);
+            youTubePlayer.loadVideo(video);
+
+        } else {
+            //No quedan videos por reproducir!!
+        }
+    }
+
     @Override
     public void onDialogPositiveClick(DialogFragment dialog) {
-        EditText edit = (EditText)dialog.getDialog().findViewById(R.id.fruit);
+        EditText edit = (EditText) dialog.getDialog().findViewById(R.id.fruit);
         String text = edit.getText().toString();
         adapter.add(text);
     }
@@ -131,4 +152,48 @@ public class MainActivity extends AppCompatActivity implements NewFruitDialogFra
     }
 
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if (!b) {
+            this.youTubePlayer=youTubePlayer;
+            youTubePlayer.setPlayerStateChangeListener(this);
+            nextVideo();
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+
+
+    @Override
+    public void onLoading() {
+
+    }
+
+    @Override
+    public void onLoaded(String s) {
+
+    }
+
+    @Override
+    public void onAdStarted() {
+
+    }
+
+    @Override
+    public void onVideoStarted() {
+
+    }
+
+    @Override
+    public void onVideoEnded() {
+        nextVideo();
+    }
+
+    @Override
+    public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+    }
 }
