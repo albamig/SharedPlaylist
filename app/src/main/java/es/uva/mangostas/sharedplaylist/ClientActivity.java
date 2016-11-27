f174a760bf8c9bc80a944ce7ddcd6f451d971e95
oscar90y5 <oscarfernandezangulo@gmail.com>
2016-11-27 15:35:41 +0100

    Añadida la actividad inicial

---
+++ app/src/main/java/es/uva/mangostas/sharedplaylist/ClientActivity.java
@@ -0,0 +1,8 @@
+package es.uva.mangostas.sharedplaylist;
+
+/**
+ * Created by oscar on 27/11/16.
+ */
+
+public class ClientActivity {
+}
---
+++ app/src/main/java/es/uva/mangostas/sharedplaylist/ServerActivity.java
@@ -0,0 +1,221 @@
+package es.uva.mangostas.sharedplaylist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+
+import java.util.ArrayList;
+
+import es.uva.mangostas.sharedplaylist.Model.ShpMediaObject;
+import es.uva.mangostas.sharedplaylist.Model.ShpSong;
+import es.uva.mangostas.sharedplaylist.Model.ShpVideo;
+
+public class ServerActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener, YouTubePlayer.PlayerStateChangeListener {
+    private ListView listView;
+    private ArrayAdapter<ShpMediaObject> adapter;
+    private YouTubePlayer yTPlayer;
+    private ArrayList<ShpMediaObject> playList;
+    private YouTubePlayerFragment youTubePlayerFragmen;
+    private ShpPlayer shpPlayerFragment;
+    private ShpMediaObject now;
+    private Boolean isIni = false;
+    private String APIKEY = "AIzaSyASYbIO42ecBEzgB5kiPpu2OHJV8_5ulnk"; //SERGIO DIJO ALGO DEL MANIFIESTO (IGUAL HAY QUE MOVER ESTO)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.server);
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.listView);
+
+        // Defined Array playList to show in ListView
+        playList = new ArrayList<ShpMediaObject>();
+
+        ;
+
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        adapter = new ArrayAdapter<ShpMediaObject>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, playList);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String itemValue = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "En desarrollo...", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //Inicializamos el fragmento
+        youTubePlayerFragmen = (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtubeFragment);
+
+        //shpPlayerFragment = getFragmentManager().findFragmentById(R.id.youtubeFragment);
+
+        //Lo escondemos hasta que le llegue algo que reproducir
+        //getFragmentManager().beginTransaction().hide(youTubePlayerFragmen).commit();
+        //getFragmentManager().beginTransaction().hide(shpPlayerFragment).commit();
+
+        //Inicializamos el reproductor de Youtube (SOLO SI SE EMPIEZA CON VIDEOS EN LA LISTA)
+        //youTubePlayerFragmen.initialize("AIzaSyASYbIO42ecBEzgB5kiPpu2OHJV8_5ulnk", this);
+        adapter.add(new ShpVideo("OBXRJgSd-aU"));
+        adapter.add(new ShpVideo("0rEVwwB3Iw0"));
+        adapter.add(new ShpSong("Melendi cream"));
+
+        nextSong();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Este metodo llama a los reprouctores depoendiendo del tipo
+     * de cancion que hay en la primera posicion de la lista.
+     */
+    public void nextSong() {
+        //Comprobamos que la lista no esta vacia
+        if(!adapter.isEmpty()) {
+            //Si el elemento es de tipo video lanzamos el youtube
+            if(adapter.getItem(0) instanceof ShpVideo) {
+                if(!isIni) {
+                    getFragmentManager().beginTransaction().show(youTubePlayerFragmen).commit();
+                    youTubePlayerFragmen.initialize(APIKEY, this);
+                    isIni = true;
+                } else {
+                    yTPlayer.loadVideo(((ShpVideo) adapter.getItem(0)).getYtCode());
+                    adapter.remove(adapter.getItem(0));
+                }
+                //Si no es un video lanzamos el reproductor propio y liberamos los recursoso del yt
+            } else {
+                getFragmentManager().beginTransaction().hide(youTubePlayerFragmen).commit();
+                yTPlayer.release();
+                isIni = false;
+                ShpSong song;
+                song = (ShpSong)adapter.getItem(0);
+                Intent intent = new Intent(ServerActivity.this, SongActivity.class);
+                startActivity(intent);
+                adapter.remove(adapter.getItem(0));
+                nextSong();
+            }
+        } else {
+            getFragmentManager().beginTransaction().hide(youTubePlayerFragmen).commit();
+            Toast.makeText(getApplicationContext(), "Fin de la lista de reproducción de YouTube", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //se llama a este metodo al inicializar el reproductor con exito
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if (!b) {
+            if(youTubePlayer != null) {
+                this.yTPlayer=youTubePlayer;
+            }
+
+            //le ponemos un controlador de cambios de estado al reproductor
+            yTPlayer.setPlayerStateChangeListener(this);
+            //reproducimos el primer video
+            ShpVideo video;
+            video = (ShpVideo)adapter.getItem(0);
+            adapter.remove(video);
+            yTPlayer.loadVideo(video.getYtCode());
+        }
+    }
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+    @Override
+    public void onLoading() {
+
+    }
+    @Override
+    public void onLoaded(String s) {
+
+    }
+    @Override
+    public void onAdStarted() {
+
+    }
+    @Override
+    public void onVideoStarted() {
+
+    }
+    @Override
+    public void onVideoEnded() {
+        //cuando se acaba un video reproducimos el siguiente
+        //Toast.makeText(getApplicationContext(), "Cambiando de tema...", Toast.LENGTH_LONG).show();
+        nextSong();
+    }
+    @Override
+    public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+    }
+}
