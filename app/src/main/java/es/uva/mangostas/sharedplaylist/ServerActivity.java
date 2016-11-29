e05849108b7aa0e683905f3e58b55dbe26c5949e
Alberto Amigo <albamigh@gmail.com>
2016-11-29 13:36:33 +0100

    Funciona el fab
    Falta moverlo abajo a la derecha

--- app/src/main/java/es/uva/mangostas/sharedplaylist/ServerActivity.java
+++ app/src/main/java/es/uva/mangostas/sharedplaylist/ServerActivity.java
@@ -2,11 +2,10 @@ package es.uva.mangostas.sharedplaylist;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -83,7 +82,8 @@ public class ServerActivity extends AppCompatActivity implements YouTubePlayer.O
         // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        com.getbase.floatingactionbutton.FloatingActionsMenu
+                fab = (com.getbase.floatingactionbutton.FloatingActionsMenu) findViewById(R.id.menu_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
