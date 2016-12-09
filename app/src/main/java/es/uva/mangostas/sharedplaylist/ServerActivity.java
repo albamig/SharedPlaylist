846aa8544b0cb4d77abe5071fec87a3e18829f77
oscar90y5 <oscarfernandezangulo@gmail.com>
2016-12-09 17:38:50 +0100

    Guarda y recupera el estado de youtube al cerrar la aplicacion

--- app/src/main/java/es/uva/mangostas/sharedplaylist/ServerActivity.java
+++ app/src/main/java/es/uva/mangostas/sharedplaylist/ServerActivity.java
@@ -31,8 +31,6 @@ import com.google.api.services.youtube.model.SearchListResponse;
 import com.quinny898.library.persistentsearch.SearchBox;
 import com.quinny898.library.persistentsearch.SearchResult;
 
-import org.apache.commons.lang3.ObjectUtils;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -73,132 +71,17 @@ public class ServerActivity extends AppCompatActivity implements YouTubePlayer.O
     private YouTubePlayerFragment youTubePlayerFragmen;
     private Boolean isIni = false;
     private String APIKEY = "AIzaSyASYbIO42ecBEzgB5kiPpu2OHJV8_5ulnk";
+    private int currentTime = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.server);
 
-        search = (SearchBox) findViewById(R.id.searchbox);
         toolbar = (Toolbar) findViewById(R.id.appBarLayout);
         this.setSupportActionBar(toolbar);
 
-        fab = (FloatingActionsMenu) findViewById(R.id.menu_fab);
-        fab_yt = (FloatingActionButton) findViewById(R.id.action_yt);
-        fab_yt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                browseOnYoutubeByTerm();
-            }
-        });
-
-        search.setSearchListener(new SearchBox.SearchListener() {
-
-            @Override
-            public void onSearchOpened() {
-                fab.collapse();
-            }
-
-            @Override
-            public void onSearchClosed() {
-                closeSearch();
-            }
-
-            @Override
-            public void onSearchTermChanged(final String term) {
-                // React to the search term changing
-                // Called after it has updated results
-
-                Log.d("ytSearch", "Empiezo la busqueda");
-
-                try {
-                    SearchListResponse searchResponse = new AsyncTask<Void, Void, SearchListResponse>() {
-                        @Override
-                        protected SearchListResponse doInBackground(Void... voids) {
-
-                            YouTube youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(),
-                                    new HttpRequestInitializer() {
-                                        public void initialize(HttpRequest request) throws IOException {
-                                        }
-                                    }).setApplicationName("ytst-150316")
-                                    .build();
-
-                            Log.d("testYT", "Inicializado el YouTube");
-
-                            try {
-                                YouTube.Search.List searchYt = youtube.search().list("id,snippet");
-                                searchYt.setKey(APIKEY);
-                                searchYt.setQ(term);
-                                searchYt.setType("video");
-                                searchYt.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
-                                searchYt.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
-
-                                Log.d("testYT", "Metida la info al objeto de consulta");
-                                SearchListResponse searchResponse = searchYt.execute();
-                                Log.d("testYT", "He realizado la consulta con exito");
-
-                                /*List<com.google.api.services.youtube.model.SearchResult> searchResultList = searchResponse.getItems();
-                                Iterator<com.google.api.services.youtube.model.SearchResult> iteratorSearchResults = searchResultList.iterator();*/
-
-                                Log.d("testYT", "Procesada la petición. ¡Tengo la info!");
-
-                                return searchResponse;
-
-    /*                            while (iteratorSearchResults.hasNext()) {
-                                    com.google.api.services.youtube.model.SearchResult singleVideo = iteratorSearchResults.next();
-                                    //ResourceId rId = singleVideo.getId();
-
-                                    SearchResult result = new SearchResult(singleVideo.getSnippet().getTitle());
-                                    search.addSearchable(result);
-
-                                   // Log.d("testYT", "Titulo: " + singleVideo.getSnippet().getTitle());
-                                }*/
-                            } catch (IOException e) {
-                                Log.d("testYT", "Estoy tirando la IOException");
-                            }
-
-                            return null;
-                        }
-
-                    }.execute((Void) null).get();
-
-                    List<com.google.api.services.youtube.model.SearchResult> searchResultList = searchResponse.getItems();
-                    Iterator<com.google.api.services.youtube.model.SearchResult> iteratorSearchResults = searchResultList.iterator();
-
-                    while (iteratorSearchResults.hasNext()) {
-                        com.google.api.services.youtube.model.SearchResult singleVideo = iteratorSearchResults.next();
-                        //ResourceId rId = singleVideo.getId();
-
-                        SearchResult result = new SearchResult(singleVideo.getSnippet().getTitle());
-                        search.addSearchable(result);
-
-                        Log.d("testYT", "Titulo: " + singleVideo.getSnippet().getTitle());
-                    }
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onSearch(final String searchTerm) {
-
-            }
-
-            @Override
-            public void onResultClick(SearchResult result) {
-                //React to result being clicked
-                Log.d("ytSearch", "Result");
-            }
-
-            @Override
-            public void onSearchCleared() {
-
-            }
-
-        });
 
         // Get ListView object from xml
         listView = (ListView) findViewById(R.id.listView);
@@ -247,16 +130,12 @@ public class ServerActivity extends AppCompatActivity implements YouTubePlayer.O
         //Inicializamos el fragmento
         youTubePlayerFragmen = (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtubeFragment);
 
-        //shpPlayerFragment = getFragmentManager().findFragmentById(R.id.youtubeFragment);
 
-        //Lo escondemos hasta que le llegue algo que reproducir
-        //getFragmentManager().beginTransaction().hide(youTubePlayerFragmen).commit();
-        //getFragmentManager().beginTransaction().hide(shpPlayerFragment).commit();
-
-        File appState = new File(getCacheDir(),"appState");
-        if(appState.exists()) {
+        File appState = new File(getApplicationContext().getCacheDir(),"appState");
+        Log.d("OSCAR","Comprobar si existe: "+appState.exists());
+        if(!appState.exists()) {
+            Log.d("OSCAR","no existe");
             //Inicializamos el reproductor de Youtube (SOLO SI SE EMPIEZA CON VIDEOS EN LA LISTA)
-            //youTubePlayerFragmen.initialize("AIzaSyASYbIO42ecBEzgB5kiPpu2OHJV8_5ulnk", this);
             adapter.add(new ShpVideo("OBXRJgSd-aU"));
             adapter.add(new ShpVideo("0rEVwwB3Iw0"));
             //adapter.add(new ShpSong("/storage/emulated/0/Music/C. Tangana - 10_15 (2015)/1 C.H.I.T.O..mp3"));
@@ -269,70 +148,185 @@ public class ServerActivity extends AppCompatActivity implements YouTubePlayer.O
     }
 
     @Override
+    protected void onResume(){
+        super.onResume();
+
+        loadState();
+
+        nextSong();
+    }
+
+    @Override
     protected void onPause(){
         super.onPause();
 
-        //Guardar lista de reproduccion
-        File appState = new File(getCacheDir(),"appState");
+        saveState();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        deleteState();
+    }
+
+    /**
+     * Guardamos el estado de la aplicacion en la cache.
+     */
+    private void saveState(){
+        //Apuntamos al fichero en el que vamos a guardar el estado
+        File appState = new File(getApplicationContext().getCacheDir(),"appState");
+        Log.d("OSCAR",getApplicationContext().getCacheDir().toString());
         try {
             FileWriter fw = new FileWriter(appState);
             PrintWriter pw = new PrintWriter(fw);
-            for(int i = 0; i< playList.size(); i++){
-                if(playList.get(i) instanceof ShpVideo){
-                    pw.print("V"+((ShpVideo) playList.get(i)).getYtCode());
+            //Anotamos en que punto de la reproduccion se encuentra la cancion en este momento.
+            if(playList.get(0) instanceof ShpVideo){
+
+                pw.print(((yTPlayer.getCurrentTimeMillis() == -1) ? 0 : yTPlayer.getCurrentTimeMillis())+"\n");
+
+                Log.d("OSCAR","save: "+yTPlayer.getCurrentTimeMillis());
+            } else {
+                pw.print(myMediaPlayer.getCurrentPosition()+"\n");
+            }
+
+            //recorremos la lista de reproduccion guardando el codigo de las canciones en la cache.
+            while(!adapter.isEmpty()){
+
+                if(adapter.getItem(0) instanceof ShpVideo){
+                    pw.print("V"+((ShpVideo) adapter.getItem(0)).getYtCode()+'\n');
+                    Log.d("OSCAR","save: "+((ShpVideo) adapter.getItem(0)).getYtCode());
+                    adapter.remove(adapter.getItem(0));
                 } else {
-                    pw.print(((ShpSong) playList.get(i)).getPath());
+                    pw.print(((ShpSong) adapter.getItem(0)).getPath()+'\n');
+                    adapter.remove(adapter.getItem(0));
                 }
             }
             fw.close();
         } catch (IOException e) {
             Log.e("ERROR","error reading file", e);
         }
-
     }
 
-   protected void onResume(){
-       super.onResume();
+    /**
+     * Cargamos el estado de la aplicacion almacenado en la cache.
+     */
+    private void loadState(){
 
-       File appState = new File(getCacheDir(),"appState");
-       //Recuperar lista
-       if(appState.exists()) {
+        File appState = new File(getApplicationContext().getCacheDir(),"appState");
+        Log.d("OSCAR","Comprobar si existe (onResume)"+appState.toString()+": "+appState.exists());
+        Log.d("OSCAR",getApplicationContext().getCacheDir().toString());
+
+        //Comprobamos que hay un estado guardado en la cache
+        if(appState.exists()) {
+            Log.d("OSCAR","existe");
 
 
-           try {
+            try {
                 FileReader fr = new FileReader(appState);
+                Log.d("OSCAR","fr");
                 BufferedReader br = new BufferedReader(fr);
-                // Lectura del fichero
+                Log.d("OSCAR","br");
+
+                //recuperamos el tiempo por el que se llegaba la priemra cancion.
                 String linea;
+                Log.d("OSCAR","1");
+                linea=br.readLine();
+                Log.d("OSCAR","2"+linea);
+                currentTime = Integer.parseInt(linea);
+                Log.d("OSCAR","current");
+
+                //recuperamos la lista de reproduccion completa
+
                 while ((linea = br.readLine()) != null) {
-                    if (linea.substring(0, 1) == "V") {
+                    Log.d("OSCAR","load");
+                    if (linea.substring(0, 1).equals("V")) {
                         adapter.add(new ShpVideo(linea.substring(1)));
                     } else {
                         adapter.add(new ShpSong(linea));
                     }
                 }
+
                 fr.close();
-           } catch (FileNotFoundException e) {
+            } catch (FileNotFoundException e) {
                 Log.e("ERROR", "file not found", e);
-           } catch (IOException e) {
+            } catch (IOException e) {
                 Log.e("ERROR", "error writing file", e);
-           }
-           appState.delete();
+            }
 
+            //Borramos el fichero de la cache.
+            appState.delete();
 
-       }
+        }
+    }
 
-       nextSong();
+    /**
+     * Eliminamos de la cache el fichero que almacena el estado de la aplicacion.
+     * @return resultado del borrado del fichero.
+     */
+    private boolean deleteState(){
+        File appState = new File(getApplicationContext().getCacheDir(),"appState");
+        return appState.delete();
+    }
+
+    /**
+     * Este metodo llama a los reprouctores depoendiendo del tipo
+     * de cancion que hay en la primera posicion de la lista.
+     */
+    public void nextSong() {
+        //Comprobamos que la lista no esta vacia
+        if(!adapter.isEmpty()) {
+
+            //Si el elemento es de tipo video lanzamos el youtube
+            if(adapter.getItem(0) instanceof ShpVideo) {
+                if(!isIni) {
+                    getFragmentManager().beginTransaction().show(youTubePlayerFragmen).commit();
+                    youTubePlayerFragmen.initialize(APIKEY, this);
+                    isIni = true;
+                } else {
+                    yTPlayer.cueVideo(((ShpVideo) adapter.getItem(0)).getYtCode(),currentTime);
+                    yTPlayer.play();
+                }
+                //Si no es un video lanzamos el reproductor propio y liberamos los recursoso del yt
+            } else {
+                getFragmentManager().beginTransaction().hide(youTubePlayerFragmen).commit();
+                yTPlayer.release();
+                isIni = false;
+                ShpSong song;
+                song = (ShpSong)adapter.getItem(0);
+                try{
+                    myMediaPlayer.setDataSource(song.getPath());
+                    myMediaPlayer.prepare();
+                    seekTo(currentTime);
+                    myMediaPlayer.start();
+
+                    myMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            myMediaPlayer.reset();
+                            myMediaController.hide();
+
+                            adapter.remove(adapter.getItem(0));
+                            currentTime = 0;
+
+                            nextSong();
+
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                /*Intent intent = new Intent(ServerActivity.this, SongActivity.class);
+                startActivity(intent);
+                adapter.remove(adapter.getItem(0));*/
+            }
+        } else {
+            getFragmentManager().beginTransaction().hide(youTubePlayerFragmen).commit();
+            Toast.makeText(getApplicationContext(), "Fin de la lista de reproducción.", Toast.LENGTH_LONG).show();
+        }
     }
 
 
-    private void browseOnYoutubeByTerm() {
-        search.revealFromMenuItem(R.id.action_yt, this);
-    }
-
-    protected void closeSearch() {
-        search.hideCircularly(this);
-    }
 
     /**
      * Metodo para preparar el media player para reproducir canciones
@@ -383,72 +377,26 @@ public class ServerActivity extends AppCompatActivity implements YouTubePlayer.O
     }
 
 
-    /**
-     * Este metodo llama a los reprouctores depoendiendo del tipo
-     * de cancion que hay en la primera posicion de la lista.
-     */
-    public void nextSong() {
-        //Comprobamos que la lista no esta vacia
-        if(!adapter.isEmpty()) {
-            //Si el elemento es de tipo video lanzamos el youtube
-            if(adapter.getItem(0) instanceof ShpVideo) {
-                if(!isIni) {
-                    getFragmentManager().beginTransaction().show(youTubePlayerFragmen).commit();
-                    youTubePlayerFragmen.initialize(APIKEY, this);
-                    isIni = true;
-                } else {
-                    yTPlayer.loadVideo(((ShpVideo) adapter.getItem(0)).getYtCode());
-                    adapter.remove(adapter.getItem(0));
-                }
-                //Si no es un video lanzamos el reproductor propio y liberamos los recursoso del yt
-            } else {
-                getFragmentManager().beginTransaction().hide(youTubePlayerFragmen).commit();
-                yTPlayer.release();
-                isIni = false;
-                ShpSong song;
-                song = (ShpSong)adapter.getItem(0);
-                try{
-                    myMediaPlayer.setDataSource(song.getPath());
-                    myMediaPlayer.prepare();
-                    myMediaPlayer.start();
 
-                    myMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                        @Override
-                        public void onCompletion(MediaPlayer mp) {
-                            myMediaPlayer.reset();
-                            myMediaController.hide();
-                            adapter.remove(adapter.getItem(0));
-                            nextSong();
-
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                /*Intent intent = new Intent(ServerActivity.this, SongActivity.class);
-                startActivity(intent);
-                adapter.remove(adapter.getItem(0));*/
-            }
-        } else {
-            getFragmentManager().beginTransaction().hide(youTubePlayerFragmen).commit();
-            Toast.makeText(getApplicationContext(), "Fin de la lista de reproducción.", Toast.LENGTH_LONG).show();
-        }
-    }
 
     //se llama a este metodo al inicializar el reproductor con exito
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        Log.d("OSCAR","inicializando");
         if (!b) {
+            Log.d("OSCAR","!b");
             if(youTubePlayer != null) {
                 this.yTPlayer=youTubePlayer;
             }
 
             //le ponemos un controlador de cambios de estado al reproductor
             yTPlayer.setPlayerStateChangeListener(this);
+
             //reproducimos el primer video
             ShpVideo video;
-            video = (ShpVideo)adapter.getItem(0);
-            yTPlayer.loadVideo(video.getYtCode());
+            video = (ShpVideo) adapter.getItem(0);
+            yTPlayer.cueVideo(video.getYtCode(),currentTime);
+            yTPlayer.play();
         }
     }
 
@@ -473,11 +421,16 @@ public class ServerActivity extends AppCompatActivity implements YouTubePlayer.O
     public void onVideoStarted() {
 
     }
+
+    /**
+     * Se ejecuta cuando finaliza un video de youTube. Se borra la cancion que se estaba
+     * reproduciendo, se pone el tiempo de reproduccion a cero y se inicia la siguiente cancion.
+     */
     @Override
     public void onVideoEnded() {
-        //cuando se acaba un video reproducimos el siguiente
-        //Toast.makeText(getApplicationContext(), "Cambiando de tema...", Toast.LENGTH_LONG).show();
         adapter.remove(adapter.getItem(0));
+        currentTime = 0;
+
         nextSong();
     }
     @Override
